1088e9212748650ea597cbe4b09edb0db3320c13
GoudaSrikanta <131533051+GoudaSrikanta@users.noreply.github.com>
2023-04-24 17:47:22 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # DataScienceEcosystem
+# 
+
+# Data Science Tools and Ecosystem
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Julia
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. scikit-learn
+# 2. TensorFlow
+# 3. Pytorch
+# 4. Theano
+
+# |Data Science Tool|
+# |-----|
+# |SAS|
+# |ApacheSpark|
+# |MATLAB|
+# |Excel|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[3]:
+
+
+#we will take some value and calculate then like multiply and adding
+(3*4)+5
+
+
+# In[4]:
+
+
+# Input: 200 minutes
+minutes = 200
+
+# Conversion
+hours = minutes / 60
+
+# Output
+print("200 minutes is equal to", hours, "hours.")
+
+
+# ## Author
+# 
+# Srikanta Gouda
